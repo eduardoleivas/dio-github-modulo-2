24567eaa9bd2028da75ba5c4f5b86c1d3c85c661
Eduardo Pinheiro <120347105+eduardoleivas@users.noreply.github.com>
2024-06-22 22:13:38 -0300

    project organization

--- concatenando-dados-copilot.py
+++
@@ -1,9 +0,0 @@
-# Solicita as duas strings ao usuário
-string1 = input("Digite a primeira string: ")
-string2 = input("Digite a segunda string: ")
-
-# Concatena as duas strings usando o operador de adição (+)
-resultado_concatenado = string1 + string2
-
-# Imprime o resultado concatenado
-print("Strings concatenadas:", resultado_concatenado)
